7d8eb154159bc1f788a5ad7e6ec63a47c710d77f
Bvhnandan <168331840+Bvhnandan@users.noreply.github.com>
2024-06-16 23:26:53 +0530

    Update Login.js

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,38 +1,38 @@
-import React,{useState,useContext} from 'react'
+import React, { useState, useContext } from 'react';
 import axios from 'axios';
-import {store} from './App';
-import { Redirect } from 'react-router';
+import { store } from './App';
+import { Navigate } from 'react-router-dom';
 
 const Login = () => {
-    const [token,setToken] = useContext(store)
-    const [data,setData] = useState({
-        email:'',
-        password:'',
-    })
-    const changeHandler = e =>{
-        setData({...data,[e.target.name]:e.target.value})
-    }
-    const submitHandler = e =>{
+    const [token, setToken] = useContext(store);
+    const [data, setData] = useState({
+        email: '',
+        password: '',
+    });
+    const changeHandler = e => {
+        setData({ ...data, [e.target.name]: e.target.value });
+    };
+    const submitHandler = e => {
         e.preventDefault();
-        axios.post('http://localhost:5000/login',data).then(
+        axios.post('http://localhost:5000/login', data).then(
             res => setToken(res.data.token)
-        )
-    }
-    if(token){
-       return <Redirect to='/myprofile' />
+        );
+    };
+    if (token) {
+        return <Navigate to='/myprofile' />;
     }
     return (
         <div>
             <center>
-            <form onSubmit={submitHandler} autocomplete="off">
-                <h3>Login</h3>
-                <input type="email" onChange={changeHandler} name="email" placeholder="Email" /><br />
-                <input type="password" onChange={changeHandler} name="password" placeholder="Password" /><br />
-                <input type="submit" value="Login" /><br />
-            </form>
+                <form onSubmit={submitHandler} autoComplete="off">
+                    <h3>Login</h3>
+                    <input type="email" onChange={changeHandler} name="email" placeholder="Email" /><br />
+                    <input type="password" onChange={changeHandler} name="password" placeholder="Password" /><br />
+                    <input type="submit" value="Login" /><br />
+                </form>
             </center>
         </div>
-    )
-}
+    );
+};
 
-export default Login
+export default Login;
