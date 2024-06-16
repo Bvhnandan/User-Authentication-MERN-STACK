b8be88afce3b23eba44534a4d58042fd604d98b1
Bvhnandan <168331840+Bvhnandan@users.noreply.github.com>
2024-06-16 23:26:21 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
-import React,{useState,createContext} from 'react'
-import {BrowserRouter,Route,Switch} from 'react-router-dom';
+import React, { useState, createContext } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Nav from './Nav';
 import Register from './Register';
 import Login from './Login';
@@ -8,21 +8,21 @@ import Myprofile from './Myprofile';
 export const store = createContext();
 
 const App = () => {
-  const [token,setToken] = useState(null);
+  const [token, setToken] = useState(null);
   return (
     <div>
-      <store.Provider value={[token,setToken]}>
-      <BrowserRouter>
-        <Nav />
-        <Switch>
-          <Route path='/register' component={Register} />
-          <Route path='/login' component={Login} />
-          <Route path='/myprofile' component={Myprofile} />
-        </Switch>
-      </BrowserRouter>
+      <store.Provider value={[token, setToken]}>
+        <BrowserRouter>
+          <Nav />
+          <Routes>
+            <Route path='/register' element={<Register />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/myprofile' element={<Myprofile />} />
+          </Routes>
+        </BrowserRouter>
       </store.Provider>
     </div>
   )
 }
 
-export default App
+export default App;
